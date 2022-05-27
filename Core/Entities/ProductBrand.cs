24f578a903367c69554fafbb6b81500aa1b4e475
bufebaa <bufebaa@gmail.com>
2022-05-27 16:34:57 +0300

    new class

---
+++ Core/Entities/ProductBrand.cs
@@ -0,0 +1,9 @@
+using Core.Entities;
+
+namespace COre.Entities
+{
+    public class ProductBrand : BaseEntity
+    {
+        public string Name { get; set; }
+    }
+}
---
+++ Core/Entities/ProductType.cs
@@ -0,0 +1,9 @@
+using Core.Entities;
+
+namespace COre.Entities
+{
+    public class ProductType : BaseEntity
+    {
+        public string Name { get; set; }
+    }
+}
